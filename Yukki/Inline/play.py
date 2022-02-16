52fcc6020941496b28b0e5dce03e31d8672dc59d
boykaalex01 <99303342+boykaalex01@users.noreply.github.com>
2022-02-16 05:39:31 +0200

    Add files via upload

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -12,11 +12,11 @@ def url_markup(videoid, duration, user_id, query, query_type):
                 callback_data=f"slider B|{query_type}|{query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="تشغيل اغنية 🎵",
+                text="اغنية",
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="تشغيل فيديو 🎥",
+                text="فيديو",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
